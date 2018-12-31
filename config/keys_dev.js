b28a78dd9d5f01530abc5f620f3d0e615504fe6c
deanban <pbp2005@gmail.com>
2018-12-30 19:04:28 -0500

    ready for deploy

--- config/keys_dev.js
+++ config/keys_dev.js
@@ -1,5 +1,6 @@
 const Mlab = require("./mlab");
 module.exports = {
-  mongoURI: `mongodb://dean:partha87@ds245234.mlab.com:45234/url-shortener-microservice`,
+  mongoURI:
+    "mongodb://dean:partha87@ds245234.mlab.com:45234/url-shortener-microservice",
   secretKey: "secret"
 };
