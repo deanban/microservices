7059c4a0b5b08422d6cc7bfcf6ec07d88e182470
deanban <pbp2005@gmail.com>
2018-12-30 22:44:32 -0500

    added environment variables

--- config/keys_dev.js
+++ config/keys_dev.js
@@ -1,6 +1,7 @@
 const Mlab = require("./mlab");
 module.exports = {
-  mongoURI:
-    "mongodb://dean:partha87@ds245234.mlab.com:45234/url-shortener-microservice",
+  mongoURI: `mongodb://${Mlab.name}:${
+    Mlab.password
+  }@ds245234.mlab.com:45234/url-shortener-microservice`,
   secretKey: "secret"
 };
